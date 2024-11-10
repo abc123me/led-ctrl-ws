5f6b5213d6274cd3b3c499320bca6b2d954fda30
jeremiah <jeremiahbl@protonmail.com>
2024-11-10 03:10:19 -0600

    improved code

--- led_strip.py
+++ led_strip.py
@@ -1,6 +1,15 @@
-import atexit, rpi_ws281x
+import atexit, rpi_ws281x, random
 
 from rpi_ws281x import Color
+
+def parseColor(str):
+	ss = str.split(',')
+	if len(ss) == 3:
+		return Color(int(ss[0], 0), int(ss[1], 0), int(ss[2], 0))
+	elif len(ss) == 4:
+		return Color(int(ss[0], 0), int(ss[1], 0), int(ss[2], 0), int(ss[3], 0))
+	else:
+		raise ValueErrro('Invalid color format used')
 
 class LedStrip:
 	strip = None
@@ -9,59 +18,114 @@ class LedStrip:
 	animation_id = 0
 	animation_ctr = 0
 
-	def progress_animation(self):
-# 		print(self.animation_id)
+	def random_colors_anim(self):
+		pass
+
+
+	temperatures = None
+	def fire_anim(self):
 		s = self.strip
-
-		if self.animation_id == 0: # Static (singular color)
-			self.clear(Color(255, 0, 255))
-		elif self.animation_id == 1: # Rainbow
-			if self.animation_ctr > 255:
-				self.animation_ctr = 0
-
-			for i in range(s.numPixels()):
-				s.setPixelColor(i, wheel((i + self.animation_ctr) & 255))
+		l = s.numPixels()
+		self.animation_ctr += 1
+		if self.temperatures:
+			# Generate some sparks
+			sparkCnt = random.randrange(3)
+			for i in range(sparkCnt):
+				self.temperatures[random.randrange(l)] = random.randrange(100) + 155
+			# Simulate the fire
+			for i in range(l):
+				temp = self.temperatures[i]
+				if temp > 0:
+					# Simulate cooling
+					temp -= 10 if temp > 100 else (5 if temp > 50 else 1)
+					# Update the array
+					self.temperatures[i] = temp
+				# Update LED with color
+				if temp > 170:
+					temp = (temp - 170) * 3
+					if temp > 255: temp = 255
+					s[i] = Color(255, 255, temp)
+				elif temp > 85:
+					temp = (temp - 85) * 3
+					if temp > 255: temp = 255
+					s[i] = Color(255, temp, 0)
+				else:
+					temp = temp * 3
+					if temp > 255: temp = 255
+					s[i] = Color(temp, 0, 0)
 			s.show()
+		else:
+			self.temperatures = [0] * l
+			self.animation_ctr = 0
+			return 0
+		return 0.1
 
-			self.animation_ctr += 1
-			return 0.02
-		elif self.animation_id == 2: # 3 bit color wipe
-			if self.animation_ctr > 7:
-				self.animation_ctr = 1
+	# Color wipe animation
+	def color_wipe_anim(self):
+		if self.animation_ctr > 7:
+			self.animation_ctr = 1
+		self.clear(Color(255 if (self.animation_ctr & 1) > 0 else 0,
+		                 255 if (self.animation_ctr & 2) > 0 else 0,
+		                 255 if (self.animation_ctr & 4) > 0 else 0))
+		self.animation_ctr += 1
+		return 0.5
 
-			self.clear(Color(255 if (self.animation_ctr & 1) > 0 else 0,
-			                 255 if (self.animation_ctr & 2) > 0 else 0,
-			                 255 if (self.animation_ctr & 4) > 0 else 0))
-
-			self.animation_ctr += 1
-			return 0.5
-		elif self.animation_id == 3:
-			pass
-		elif self.animation_id == 4:
-			pass
-		elif self.animation_id == 5:
-			pass
+	# Static (single color) animation
+	def static_anim(self):
+		color = Color(255, 0, 0)
+		if self.animation_info["color"]:
+			color = parseColor(self.animation_info["color"])
+		else:
+			print("Warning: color not set")
+		self.clear(color)
 		return 1
 
+	# Rainbow animation
+	def rainbow_anim(self):
+		s = self.strip()
+		if self.animation_ctr > 255:
+			self.animation_ctr = 0
+		for i in range(s.numPixels()):
+			s[i] = wheel((i + self.animation_ctr) & 255)
+		s.show()
+		self.animation_ctr += 1
+		return 0.02
+
 	def __init__(self, str, clr_color, auto_clear=True):
+		self.progress_animation = self.static_anim
 		self.strip = str
 		self.clear_color = clr_color
 		self.strip.begin()
 		if auto_clear:
 			self.clear()
 		atexit.unregister(self.strip._cleanup)
+
 	def clear(self, color=None):
 		for num in range(self.strip.numPixels()):
-			self.strip.setPixelColor(num, color if color else self.clear_color)
+			self.strip[num] = color if color else self.clear_color
 		self.strip.show()
+
 	def cleanup(self):
 		self.strip._cleanup()
+
 	def set_brightness(self, b):
 		self.strip.setBrightness(b)
+
 	def set_animation(self, id, info):
 		self.animation_id = id
 		self.animation_info = info
 		self.animation_ctr = 0
+		if self.animation_id == 1:   # Rainbow
+			self.progress_animation = self.rainbow_anim
+		elif self.animation_id == 2: # Color wipe
+			self.progress_animation = self.color_wipe_anim
+		elif self.animation_id == 3: # Random colors
+			self.progress_animation = self.random_colors_anim
+		elif self.animation_id == 4: # Fire
+			self.progress_animation = self.fire_anim
+		else:                        # Static (singular color)
+			self.progress_animation = self.static_anim
+
 
 def lookup_strip_type(strip_str):
 	strip_str = str(strip_str).lower().strip()
--- leds.py
+++ leds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env /home/leds/led-ctrl-ws/leds-venv/bin/python3
 
-import atexit, time, rpi_ws281x
+import atexit, time, rpi_ws281x, json
 from led_strip import LedStrip, lookup_strip_type
 from mysql import connector as mysql
 from rpi_ws281x import PixelStrip, Color
@@ -22,24 +22,31 @@ def anim_strip(led_id, anim_id):
 	blink = False
 	cksum = -69420
 	while not shutdown:
+		# Global tableChecksum is set every second via another thread
 		if tableChecksum != cksum:
 			cksum = tableChecksum
 			leds_db_lock.acquire()
-			leds_cursor.execute("SELECT Brightness, AnimationID FROM LedStrips WHERE ID = %d" % (led_id))
+			leds_cursor.execute("SELECT Brightness, AnimationID, AnimationInfo FROM LedStrips WHERE ID = %d" % (led_id))
 			res = leds_cursor.fetchone()
-			strip.set_brightness(res[0])
-			strip.set_animation(res[1], None)
 			leds_db_lock.release()
+			print("Brightness set to %d/255" % res[0])
+			strip.set_brightness(res[0])
+			print("Animation ID set to %d" % res[1])
+			print("Animation info set to: %s" % str(res[2]))
+			strip.set_animation(res[1], json.loads(res[2]))
 			print("Animation table updated!")
 		time.sleep(strip.progress_animation())
 	print("Animation thread stopped!")
 
 def main():
-	print("Hello world, connecting to MySQL server now!")
+	# Start MySQL connection
+	print("Connecting to MySQL server now!")
 	global leds_db, leds_cursor
 	leds_db = mysql.connect(host="localhost", user="leds", password="password", database="LED_DB")
 	leds_cursor = leds_db.cursor()
 	print("MySQL Connected: " + str(leds_db))
+
+	# Poll MySQL databse for initialization info
 	global tableChecksum
 	leds_cursor.execute("CHECKSUM TABLE LedStrips")
 	tableChecksum = str(leds_cursor.fetchone()[1])
@@ -47,6 +54,8 @@ def main():
 	leds_cursor.execute("SELECT ID, Count, Pin, FreqHz, DMA, Invert, Channel, Ordering, "
 	#        8           9            10           11           12
 		"Brightness, ClearColorR, ClearColorG, ClearColorB, AnimationID FROM LedStrips")
+
+	# Initialize each LED strip
 	global led_strips, anim_threads
 	for x in leds_cursor:
 		print("Initializing LED Strip: " + str(x))
@@ -54,28 +63,44 @@ def main():
 		led_strips[x[0]] = LedStrip(PixelStrip(num=x[1], pin=x[2], freq_hz=x[3], dma=x[4], invert=x[5], channel=x[6], strip_type=lookup_strip_type(x[7]), brightness=x[8]), Color(x[9], x[10], x[11]))
 		anim_threads[x[0]] = Thread(target=anim_strip, args=(x[0], x[12]))
 		print("Initialized!")
+
 	# Start animation threads
 	global shutdown
 	for id in range(len(led_strips)):
 		anim_threads[id].start()
 	print("Animation thread(s) started!")
+
+	# Start monitoring threads
 	global leds_db_lock
 	try:
 		while True:
+			# Update global table checksum
 			leds_db_lock.acquire()
 			leds_cursor.execute("CHECKSUM TABLE LedStrips")
 			tableChecksum = str(leds_cursor.fetchone()[1])
 			leds_db.commit()
 			leds_db_lock.release()
-			time.sleep(1)
+
+			# Check on animation threads
+			for id in range(len(led_strips)):
+				if not anim_threads[id].is_alive():
+					print("An animation thread died, time to exit")
+					break
+			else:
+				# Give the CPU some free time
+				time.sleep(1)
+				continue
+			break
 	except KeyboardInterrupt:
 		print("Program interrupted, shutting down gracefully!")
+
 	# Handle exit
 	shutdown = True
 
 def on_exit():
 	print("Shutting down!")
-	# Stop animation thread, clear LED strips
+
+	# Stop animation threads, cleanup and turn off LED strips
 	global shutdown, anim_threads, led_strips
 	shutdown = True
 	for id in range(len(led_strips)):
@@ -83,12 +108,14 @@ def on_exit():
 		led_strips[id].clear()
 		led_strips[id].cleanup()
 	print("LED strips cleared!")
+
 	# Close MySQL cursor
 	global leds_cursor
 	if leds_cursor:
 		leds_cursor.close()
 		leds_cursor = None
 		print("MySQL cursor closed!")
+
 	# Close MySQL connection
 	global leds_db
 	if leds_db:
